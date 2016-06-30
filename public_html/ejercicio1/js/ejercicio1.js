86e1aef1393247da8a5bf8a381d7876b7880438e
Alumne <Alumne@10.199.25.24>
2016-06-30 13:38:05 +0200

    Inicio curso javascript

---
+++ public_html/ejercicio1/js/ejercicio1.js
@@ -0,0 +1,26 @@
+//
+///*base rectangulo*/
+//    var base = prompt("Escribe la base del rectangulo...");
+//    var altura = prompt("Escribe la altura del rectangulo...");
+//    var resultado = base * altura;
+//    alert("Area rectangulo= " + resultado);
+    
+/*perimetro rectangulo*/
+//
+//   var base=prompt("escribe la base del rectangulo..."); 
+//   var altura=prompt("escribe la altura del rectangulo...");
+//   var resultado= (base*2)+(altura*2);
+//   alert("perimetro del Rectangulo: "+resultado);
+
+/*calcular dies, horas, minutos segundos*/
+
+//var segundos = prompt("Escribe el numero de segundos");
+//var tiempo = segundos;
+//
+//var minutos = tiempo / 60;
+//var horas = minutos / 60;
+//var dias = horas / 24;
+//
+//alert("Dias: " + dias + "\nHoras: " + horas + "\nMinutos: " + minutos + "\nSegundos: " + segundos);
+
+ 
